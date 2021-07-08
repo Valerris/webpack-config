c254135f116d14c191649d29c772e3c9f285dc6d
Valerris <ya@vlrrs>
2021-07-08 15:47:26 +0300

    [fix]: convert babelrc to obj for jest pckg use

--- .babelrc.js
+++ .babelrc.js
@@ -1,34 +1,61 @@
-const path = require("path")
+const plugins = [
+	"babel-plugin-transform-imports",
+	"@babel/plugin-transform-runtime",
+]
 
-module.exports = function (api) {
-	api.cache.invalidate(() => process.env.NODE_ENV)
+process.env.STUB_API &&
+	process.env.NODE_ENV === "development" &&
+	plugins.push("react-refresh/babel")
 
-	const plugins = [
-		"babel-plugin-transform-imports",
-		"@babel/plugin-transform-runtime",
-	]
-
-	process.env.STUB_API &&
-		api.env("development") &&
-		plugins.push("react-refresh/babel")
-
-	return {
-		presets: [
-			[
-				"@babel/preset-env",
-				{
-					modules: false,
-					corejs: 3.15,
-					useBuiltIns: "usage",
-				},
-			],
-			[
-				"@babel/preset-react",
-				{
-					runtime: "automatic",
-				},
-			],
+module.exports = {
+	presets: [
+		[
+			"@babel/preset-env",
+			{
+				modules: process.env.NODE_ENV === "test" ? "commonjs" : false,
+				corejs: 3.15,
+				useBuiltIns: "usage",
+			},
 		],
-		plugins,
-	}
+		[
+			"@babel/preset-react",
+			{
+				runtime: "automatic",
+			},
+		],
+	],
+	plugins,
 }
+
+// module.exports = function (api) {
+// 	api.cache.invalidate(() => process.env.NODE_ENV)
+
+// 	const plugins = [
+// 		"babel-plugin-transform-imports",
+// 		"@babel/plugin-transform-runtime",
+// 	]
+
+// 	process.env.STUB_API &&
+// 		api.env("development") &&
+// 		plugins.push("react-refresh/babel")
+
+// 	return {
+// 		presets: [
+// 			[
+// 				"@babel/preset-env",
+// 				{
+// 					modules: api.env("test") ? "commonjs" : false,
+// 					corejs: 3.15,
+// 					useBuiltIns: "usage",
+// 				},
+// 			],
+// 			[
+// 				"@babel/preset-react",
+// 				{
+// 					runtime: "automatic",
+// 				},
+// 			],
+// 		],
+// 		plugins,
+// 	}
+// }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import "styles.css"
-// import Pic from "public/assets/pic.jpg"
-
-// console.log(Pic)
-// // ;(async function f() {
-// // 	await fetch("/test")
-// // 		.then((res) => console.log(res))
-// // 		.catch((e) => console.log(e))
-// // })()
-
-// console.log("test")
-
-// if (module.hot) {
-// 	module.hot.accept()
-// }
 
 import React from "react"
 import { render } from "react-dom"
